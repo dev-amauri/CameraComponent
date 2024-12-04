06faa72523feba248033d576837a69c78c4919ae
dev-amauri <dev.amauri.cocom@gmail.com>
2024-12-03 21:48:39 -0600

    First page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,21 @@
+import styles from './page.module.css'
+
 export default function Home() {
-  return <h1>Home</h1>;
+
+  return (
+    <div className={styles.containerPage} >
+      <div className={styles.containerColor}>
+        <div>
+        <h1 className={styles.title}>
+          Bienvenidos al Sistema OCR de E-sigen ISR
+        </h1>
+        <p className={styles.text}>
+          En representación del equipo de desarrollo, agradecemos la oportunidad de ser parte de este proyecto.
+        </p>
+        </div>
+        
+        <img src='/illustration/Esigen_Blue.png' alt='image' className={styles.image}/>
+      </div>
+    </div>
+  );
 }
