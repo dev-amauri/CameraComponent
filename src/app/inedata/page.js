f76d00015087ee8c3e034b4c8e65360186b067dd
dev-amauri <dev.amauri.cocom@gmail.com>
2024-12-04 17:19:36 -0600

    Test camera

--- src/app/inedata/page.js
+++ src/app/inedata/page.js
@@ -1,11 +1,9 @@
 import CameraINE from "@/sections/captureINE/CameraINE";
-import CameraOpenCV from "@/sections/captureINE/CameraOpenCV";
+
 export default function INEDataPage() {
   return (
     <div >
       <CameraINE />
-      <CameraOpenCV />
-
     </div>
 
   );
